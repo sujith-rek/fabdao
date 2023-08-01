0cc4b1f8dda532dd0c4250ccf47609bc8a35fc5f
sujith-rek <ps335@snu.edu.in>
2023-08-01 22:19:00 +0530

    add routing tasks

--- frontend/src/app/child/child.component.ts
+++ frontend/src/app/child/child.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ParentService } from '../services/parent.service';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit {
+
+  id : string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private parentService: ParentService
+  ) {}
+
+  ngOnInit() : void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id += params.get('id');
+    });
+  }
 
 }
