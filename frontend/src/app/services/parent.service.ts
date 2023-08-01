8506f9c5351e2667da61a71c999dc5c4009ff391
sujith-rek <ps335@snu.edu.in>
2023-08-01 20:29:47 +0530

    frontend parent service

--- frontend/src/app/services/parent.service.ts
+++ frontend/src/app/services/parent.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParentService {
+  private baseUrl : string = 'http://localhost:8080/';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // http://localhost:8080/page?currentPage=1&pageSize=2
+  getPage(page: number){
+    return this.http.get(this.baseUrl + 'page?currentPage=' + page + '&pageSize=2');
+  }
+
+  //	http://localhost:8080/child?id=1
+  getChild(id: number){
+    return this.http.get(this.baseUrl + 'child?id=' + id);
+  }
+
 }
