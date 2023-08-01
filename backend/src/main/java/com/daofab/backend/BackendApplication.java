d4e68da3183ac8ed9ce3db82d71d5e3eff77b80b
sujith-rek <ps335@snu.edu.in>
2023-08-01 19:32:59 +0530

    code format

--- backend/src/main/java/com/daofab/backend/BackendApplication.java
+++ backend/src/main/java/com/daofab/backend/BackendApplication.java
@@ -14,39 +14,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackendApplication {
 
-	private final ParentService service = new ParentService();
+    private final ParentService service = new ParentService();
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World";
-	}
+    @GetMapping("/")
+    public String hello() {
+        return "DAOFAB Backend Test";
+    }
 
-//	http://localhost:8080/parent?id=1
-	@GetMapping("/parent")
-	@ResponseBody
-	public JSONObject parent(@RequestParam(value = "id", defaultValue = "1") String id) {
-		return this.service.getParent(Integer.parseInt(id));
-	}
+    //	http://localhost:8080/parent?id=1
+    @GetMapping("/parent")
+    @ResponseBody
+    public JSONObject parent(@RequestParam(value = "id", defaultValue = "1") String id) {
+        return this.service.getParent(Integer.parseInt(id));
+    }
 
-//	http://localhost:8080/child?id=1
-	@GetMapping("/child")
-	@ResponseBody
-	public JSONObject child(@RequestParam(value = "id", defaultValue = "1") String id) {
-		return this.service.getChild(Integer.parseInt(id));
-	}
+    //	http://localhost:8080/child?id=1
+    @GetMapping("/child")
+    @ResponseBody
+    public JSONObject child(@RequestParam(value = "id", defaultValue = "1") String id) {
+        return this.service.getChild(Integer.parseInt(id));
+    }
 
-//	http://localhost:8080/page?currentPage=1&pageSize=2
-	@GetMapping("/page")
-	@ResponseBody
-	public JSONObject page(@RequestParam(value = "currentPage", defaultValue = "1") String currentPage,
-						   @RequestParam(value = "pageSize", defaultValue = "2") String pageSize) {
-		return this.service.getPage(Integer.parseInt(currentPage), Integer.parseInt(pageSize));
-	}
-
+    //	http://localhost:8080/page?currentPage=1&pageSize=2
+    @GetMapping("/page")
+    @ResponseBody
+    public JSONObject page(@RequestParam(value = "currentPage", defaultValue = "1") String currentPage,
+                           @RequestParam(value = "pageSize", defaultValue = "2") String pageSize) {
+        return this.service.getPage(Integer.parseInt(currentPage), Integer.parseInt(pageSize));
+    }
 
 
 }
